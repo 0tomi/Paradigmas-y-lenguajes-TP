5b1077a14d580369835681dba05a3dd5364f708e
tomas schlotahuer <tomsschlotahuer@hotmail.com>
2025-11-11 14:54:33 -0300

    Update ej14.exs

--- ej14.exs
+++ ej14.exs
@@ -15,3 +15,22 @@ defmodule Ej14 do
     end
 end
 IO.inspect(Ej14.eliminarOcurrencias([1,2,3,4,5], 3))
+
+# 35. Considere que cada conjunto se representa mediante una lista.
+#     Defina funciones para simular:
+#       a. Unión de conjuntos.
+#       b. Intersección de conjuntos.
+#       c. Diferencia de conjuntos.
+#       d. Diferencia simétrica de conjuntos.
+
+# 39. Escriba una función llamada "Cantidad-de" que toma como
+#     argumentos una lista y una condición (función), y
+#     devuelve la cantidad de elementos de la lista que
+#     cumplen con dicha condición.
+
+
+# 48. Escriba una función que tome una lista y devuelva una lista
+#     de pares (número frecuencia), ordenados de mayor a
+#     menor de acuerdo a esta última.
+#     La frecuencia correspondiente a cada número es la cantidad de
+#     veces que el número aparece en la lista.
